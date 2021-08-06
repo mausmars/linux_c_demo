e414a9311c9b59fe0484ccb8f9718a66d0681dcf
马里 <mali.1101@bytedance.com>
2021-08-06 16:18:18 +0800

    java堆外内存测试。。。

---
+++ src/jni_test/java_unsafe/UnsafeTest.java
@@ -0,0 +1,98 @@
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+//https://www.yasinshaw.com/articles/59
+
+/**
+ * gradle
+ * <p>
+ * compileJava{
+ * options.encoding = "UTF-8"
+ * <p>
+ * options.compilerArgs << '-XDignore.symbol.file'
+ * options.fork = true // may not needed on 1.8
+ * options.forkOptions.executable = 'javac' // may not needed on 1.8
+ * }
+ * <p>
+ * 或
+ * javac -XDignore.symbol.file UnsafeTest.java
+ */
+
+public class UnsafeTest {
+    private static Unsafe unsafe;
+
+    static {
+        unsafe = Unsafe();
+    }
+
+    private static Unsafe Unsafe() {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            return (Unsafe) field.get(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void printData(long address, int size) {
+        for (int i = 0; i < size; i += 8) {
+            byte v = unsafe.getByte(address + i);
+            System.out.print(v + ",");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        boolean isRun = true;
+        int size = 1 * 1024 * 1024;  //1m
+
+        int n = 50;
+        List<Long> addresses = new LinkedList();
+
+        String readme = "---------------------------------------------\n" +
+                "0:结束\n" +
+                "1:创建内存\n" +
+                "2:回收\n";
+        try {
+            for (; isRun; ) {
+                System.out.println(readme);
+                Scanner input = new Scanner(System.in);
+                int num = input.nextInt();
+                switch (num) {
+                    case 0:
+                        isRun = false;
+                        break;
+                    case 1:
+                        System.out.println("创建内存");
+                        for (int i = 0; i < n; i++) {
+                            long address = unsafe.allocateMemory(size);
+                            unsafe.setMemory(address, size, (byte) 0);
+                            addresses.add(address);
+                        }
+                        System.out.println("当前内存大小 size=" + addresses.size() + "m");
+                        break;
+                    case 2:
+                        System.out.println("回收全部内存");
+                        for (long addr : addresses) {
+                            unsafe.freeMemory(addr);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (OutOfMemoryError ex) {
+            System.err.println(ex);
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            System.err.println(ex);
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/jni_test/java_unsafe/unsafe_start.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+tradePortalPID=0
+MODULE_PATH=.
+APP_MAIN=UnsafeTest
+
+APP_JAVA_GC_PRINT="-XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:MaxDirectMemorySize=32m -Dsun.reflect.inflationThreshold=0 -Xloggc:$MODULE_PATH/data_gc.log"
+APP_JAVA_GC="-XX:+UseG1GC -XX:SurvivorRatio=8 -XX:MaxGCPauseMillis=100 -XX:ParallelGCThreads=8 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$MODULE_PATH/data_dump"
+APP_JAVA_OPTS="-server -Xms128m -Xmx128m -Xss512k -XX:MetaspaceSize=32m -XX:MaxMetaspaceSize=32m"
+
+getTradeProtalPID(){
+    javaps=`$JAVA_HOME/bin/jps -l | grep $APP_MAIN`
+    if [ -n "$javaps" ]; then
+        tradePortalPID=`echo $javaps | awk '{print $1}'`
+    else
+        tradePortalPID=0
+    fi
+}
+
+start(){
+    getTradeProtalPID
+    echo "==============================================================================================="
+    if [ $tradePortalPID -ne 0 ]; then
+        echo "$APP_MAIN already started(PID=$tradePortalPID)"
+        echo "==============================================================================================="
+    else
+        echo -n "Starting $APP_MAIN \n"
+        $JAVA_HOME/bin/java -Djava.ext.dirs=.:lib $APP_JAVA_OPTS $APP_JAVA_GC $APP_JAVA_GC_PRINT $APP_MAIN
+        getTradeProtalPID
+        if [ $tradePortalPID -ne 0 ]; then
+            echo "(PID=$tradePortalPID)...[Success]"
+            echo "==============================================================================================="
+        else
+            echo "[Failed]"
+            echo "==============================================================================================="
+        fi
+    fi
+}
+
+start
