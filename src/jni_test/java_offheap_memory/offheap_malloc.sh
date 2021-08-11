4d5c8eaa65a393bc934103be00cfd4de3b801a88
马里 <mali.1101@bytedance.com>
2021-08-11 14:15:44 +0800

    修改测试。。。

--- src/jni_test/java_offheap_memory/offheap_malloc.sh
+++ src/jni_test/java_offheap_memory/offheap_malloc.sh
@@ -4,6 +4,7 @@ $JAVA_HOME/bin/javac MallocTest.java
 
 export LD_PRELOAD=$JEMALLOC_HOME/lib/libjemalloc.so
 #export MALLOC_CONF="prof:true,lg_prof_sample:0,lg_prof_interval:30,prof_prefix:jeprof.out"
-export MALLOC_CONF="prof_leak:true,lg_prof_sample:0,lg_prof_interval:30,prof_final:true,prof_prefix:jeprof.out"
+#export MALLOC_CONF="prof_leak:true,lg_prof_sample:0,prof_final:true,prof_prefix:jeprof.out"
+export MALLOC_CONF="prof_leak:true,lg_prof_sample:0,prof_final:true"
 
 $JAVA_HOME/bin/java -Djava.library.path=lib MallocTest
--- src/jni_test/java_offheap_memory/offheap_unsafe.sh
+++ src/jni_test/java_offheap_memory/offheap_unsafe.sh
@@ -3,6 +3,8 @@
 $JAVA_HOME/bin/javac -XDignore.symbol.file UnsafeTest.java
 
 export LD_PRELOAD=$JEMALLOC_HOME/lib/libjemalloc.so
-export MALLOC_CONF="prof_leak:true,lg_prof_sample:0,lg_prof_interval:30,prof_final:true,prof_prefix:jeprof.out"
+#export MALLOC_CONF="prof_leak:true,lg_prof_sample:0,lg_prof_interval:30,prof_final:true,prof_prefix:jeprof.out"
+export MALLOC_CONF=prof_leak:true,lg_prof_sample:0,prof_final:true
+#export MALLOC_CONF=prof:true,lg_prof_interval:30,lg_prof_sample:17
 
 $JAVA_HOME/bin/java UnsafeTest
