088194ba1346f377634a0d7b89dc60d753443610
马里 <dalaoshuxxx@gmail.com>
2022-04-21 15:24:02 +0800

    提交lua c api demo。。。

--- src/luac_test/luac_test1.c
+++ src/luac_test/luac_test1.c
@@ -7,21 +7,21 @@ int main(void) {
     char buff[256];
     int error;
 
-    lua_State *L = luaL_newstate();                      /* opens Lua */
-//    luaL_openlibs(L);
-//    luaopen_base(L);                                /* opens the basic library */
-//    luaopen_table(L);                               /* opens the table library */
-//    luaopen_io(L);                                  /* opens the I/O library */
-//    luaopen_string(L);                              /* opens the string lib. */
-//    luaopen_math(L);                                /* opens the math lib. */
-//
-//    while (fgets(buff, sizeof(buff), stdin) != NULL) {
-//        error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
-//        if (error) {
-//            fprintf(stderr, "%s", lua_tostring(L, -1));
-//            lua_pop(L, 1);/* pop error message from the stack */
-//        }
-//    }
-//    lua_close(L);
+    lua_State *L = luaL_newstate();                 /* opens Lua */
+    luaL_openlibs(L);
+    luaopen_base(L);                                /* opens the basic library */
+    luaopen_table(L);                               /* opens the table library */
+    luaopen_io(L);                                  /* opens the I/O library */
+    luaopen_string(L);                              /* opens the string lib. */
+    luaopen_math(L);                                /* opens the math lib. */
+
+    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
+        if (error) {
+            fprintf(stderr, "%s", lua_tostring(L, -1));
+            lua_pop(L, 1);/* pop error message from the stack */
+        }
+    }
+    lua_close(L);
     return 0;
 }
---
+++ src/luac_test/readme.txt
@@ -0,0 +1,21 @@
+gcc -Wall ./example.c -llua -ldl -lm
+
+
+gcc -Wall -I/mnt/e/work/git_src/lua ./luac_test.c
+
+
+
+gcc -Wall -I/mnt/e/work/git_src/lua ./luac_test1.c
+
+
+
+gcc -Wall -I/usr/local/include/luajit-2.1 -lluajit-5.1 -ldl -lm -o luac_test luac_test.c
+
+gcc -Wall -I/usr/local/include/luajit-2.1 -lluajit-5.1 -o luac_test luac_test.c
+
+gcc -Wall -I/usr/local/include/luajit-2.1 -lluajit-5.1 -o luac_test1 luac_test1.c
+
+
+
+
+
